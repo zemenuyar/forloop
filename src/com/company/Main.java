be746a15d79f14ba9d8e4e78139f7dc5255d2502
[zemenuyar] <[zemenuyar@gmail.com]>
2018-10-04 11:21:43 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+            for(int i=10; i>1; i--)
+                System.out.println("The value of i is: "+i);
+    }
+}
